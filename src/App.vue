68a82efda35973500afb1c20f37670fe4c53f22b
weiyann <jona11105@gmail.com>
2024-03-14 14:17:24 +0800

    card css style

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,20 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import card from '@/components/fruit-card.vue'
+import { ref } from 'vue'
+import data from '../data/fruit-data.json'
+
+console.log(data)
+const fruitData = ref(data)
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <main>
+    <card />
+  </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
+/* @media (min-width: 1024px) {
   header {
     display: flex;
     place-items: center;
@@ -81,5 +39,5 @@ nav a:first-of-type {
     padding: 1rem 0;
     margin-top: 1rem;
   }
-}
+} */
 </style>
---
+++ src/components/fruit-card.vue
@@ -0,0 +1,51 @@
+<script setup></script>
+
+<template>
+  <div class="card-container">
+    <div class="card">
+      <div class="front">
+        <img src="/image/guava.png" alt="guava" class="fruit-card" />
+      </div>
+
+      <div class="back"></div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  height: 285px;
+  width: 220px;
+  position: relative;
+  transform-style: preserve-3d;
+  perspective: 1000px;
+}
+.front,
+.back {
+  height: 285px;
+  width: 220px;
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  backface-visibility: hidden;
+  transition: all 0.5s;
+}
+.front {
+  background-color: rgb(19, 0, 144);
+}
+.fruit-card {
+  height: 265px;
+  width: 200px;
+}
+.back {
+  background-color: pink;
+  transform: rotateY(180deg);
+}
+.card:hover .front {
+  transform: rotateY(180deg);
+}
+.card:hover .back {
+  transform: rotateY(0deg);
+}
+</style>
